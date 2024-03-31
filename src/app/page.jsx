1f87b5d98f321baf864100b97ce8bde82aa0afa8
arfadmuzali <arfadmuzali258@gmail.com>
2024-03-31 16:13:54 +0700

    fix: github source code url

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -21,10 +21,10 @@ export default function Home() {
           <footer className="p-5 bg-neutral-600 mt-4 text-white">
             GitHub Source Code :{" "}
             <Link
-              href={"https://github.com/arfadmuzali"}
+              href={"https://github.com/arfadmuzali/arisan-online/"}
               className="text-blue-200"
             >
-              https://github.com/arfadmuzali
+              https://github.com/arfadmuzali/arisan-online/
             </Link>
           </footer>
         </div>
